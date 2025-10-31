bd677316b8e2a2a9331052efdda5939ce5ff49d3
pranay1222 <pranaybanait221204@gmail.com>
2025-10-31 17:11:57 +0530

    update

--- app.js
+++ app.js
@@ -83,6 +83,9 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter)
 app.use("/",userRouter);
 
+app.get("/",(req,res)=>{
+    console.log("root");
+});
 
 // app.get("/demo",async(req,res)=>{
 //     const fakeUser=new User({
